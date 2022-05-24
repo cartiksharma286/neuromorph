aaaa18267b8ed44f0c9bf5ed2b27ebb31facebe4
cartiksharma286 <76185710+cartiksharma286@users.noreply.github.com>
2022-05-24 17:23:30 -0400

    Made changes to classification report calculations.

--- Dementia_Detection/RunModel.py
+++ Dementia_Detection/RunModel.py
@@ -80,20 +80,23 @@ X2=df[['EDUC', 'SES', 'nWBV', 'ASF', 'MMSE', 'CDR']]
 y2=df["Group"]
 X2=sm.add_constant(X2)
 X_train,X_test,y_train,y_test=train_test_split(X2,y2,test_size=0.3)
+from sklearn.metrics import accuracy_score
 
 logreg=sm.Logit(y_train,X_train).fit()
 logreg.summary()
 y_pred=logreg.predict(X_test)
+np.argmax(y_pred, axis=0)
 correct=(TN+TP)/(TN+TP+FP+FN)
 print("Correctly classified :",correct*100)
 in_correct=(FN+FP)/(TN+TP+FP+FN)
 print("In_Correctly classified :",in_correct*100)
 #update_score_card(algorithm_name = 'Logistic Regression -RFE', model = logreg)
 from sklearn.metrics import plot_confusion_matrix
-
+#accuracy_score(X_test, y_pred)
 #score_card
 #cm=classification_report(y_pred,y_test)
-#print(cm)
+print("here")
+print(cm)
 #plot_confusion_matrix(gnb_model)
 #cm = confusion_matrix(y_pred,y_test)
 #TP=cm[0,0]
@@ -102,7 +105,7 @@ from sklearn.metrics import plot_confusion_matrix
 #FN=cm[0,1]
 
 #acc = (TN+TP)/(TN+FP+TP+FN)
-precision = TP / (TP+FP)
+#precision = TP / (TP+FP)
 #recall = TP / (TP+FN)
 #specificity = TN / (TN+FP)
 #f1_score = 2*((precision*recall)/(precision+recall))
@@ -120,6 +123,7 @@ knn_model = knn_classification.fit(X_train, y_train)
 y_pred=knn_model.predict(X_test)
 #plot_confusion_matrix(knn_model)
 cm = confusion_matrix(y_pred,y_test)
+cm2 = classification_report(y_pred,y_test)
 TP=cm[0,0]
 TN=cm[1,1]
 FP=cm[1,0]
@@ -132,10 +136,12 @@ specificity = TN / (TN+FP)
 f1_score = 2*((precision*recall)/(precision+recall))
 
 print("knn################################################")
+print(cm2)
 print("accuracy "+str(acc))
 print("precision"+str(precision))
 print("specificity"+str(specificity))
 print("f1_score"+str(f1_score))
+print(cm)
 #update_score_card(algorithm_name = 'KNN ', model = knn_model)
 from sklearn.ensemble import GradientBoostingClassifier
 #score_card
@@ -143,6 +149,7 @@ gboost_model = GradientBoostingClassifier(n_estimators = 150, max_depth = 10, ra
 
 gboost_model.fit(X_train, y_train)
 y_pred=gboost_model.predict(X_test)
+cm2 = classification_report(y_pred,y_test)
 
 cm = confusion_matrix(y_pred, y_test)
 TP=cm[0,0]
@@ -156,12 +163,13 @@ recall = TP / (TP+FN)
 specificity = TN / (TN+FP)
 f1_score = 2*((precision*recall)/(precision+recall))
 print("gboost_model################################################")
+print(cm2)
 print("accuracy "+str(acc))
 print("precision"+str(precision))
 print("specificity"+str(specificity))
 print("f1_score"+str(f1_score))
-#plot_confusion_matrix(xgb_model)
-
+plot_confusion_matrix(gboost_model,X_test,y_test)
+print(cm)
 
 #plot_confusion_matrix(gboost_model)
 #test_report = get_test_report(gboost_model)
@@ -172,6 +180,7 @@ xgb_model = XGBClassifier(max_depth = 10, gamma = 1)
 xgb_model.fit(X_train, y_train)
 y_pred=xgb_model.predict(X_test)
 cm = confusion_matrix(y_pred, y_test)
+cm2 = classification_report(y_pred,y_test)
 TP=cm[0,0]
 TN=cm[1,1]
 FP=cm[1,0]
@@ -183,8 +192,9 @@ recall = TP / (TP+FN)
 specificity = TN / (TN+FP)
 f1_score = 2*((precision*recall)/(precision+recall))
 print("xgb_model################################################")
+print(cm2)
 print("accuracy "+str(acc))
 print("precision"+str(precision))
 print("specificity"+str(specificity))
 print("f1_score"+str(f1_score))
-#plot_confusion_matrix(xgb_model)
+plot_confusion_matrix(xgb_model,X_test,y_test)
